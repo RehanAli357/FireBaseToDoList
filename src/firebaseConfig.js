a9681dfd3d4c0c8a54768ff76b770a0330c66821
Mohd Rehan Ali <110824286+RehanAli357@users.noreply.github.com>
2023-01-14 16:36:29 +0530

    Second Commit

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,13 +1,6 @@
 import { initializeApp } from "firebase/app";
 import {getFirestore} from "@firebase/firestore";
-const firebaseConfig = {
-    apiKey: "AIzaSyAskvFWQ6rCxpBERUopMAHlpMKaFBU5brA",
-    authDomain: "crud-todo-9ac98.firebaseapp.com",
-    projectId: "crud-todo-9ac98",
-    storageBucket: "crud-todo-9ac98.appspot.com",
-    messagingSenderId: "842709463812",
-    appId: "1:842709463812:web:3d04ec1f00162d155b9651"
-  };
+import firebaseConfig from "./firebase";
   const app = initializeApp(firebaseConfig);
   const db=getFirestore();
   export default db;
